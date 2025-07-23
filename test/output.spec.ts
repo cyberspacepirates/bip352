ed2de8a41d8ea82599b76aa6de80821b59088e3e
Cyber <176347536+cyberspacepirates@users.noreply.github.com>
2025-07-23 15:16:32 -0300

    Adds(ts_src/output.ts) and test file

---
+++ test/output.spec.ts
@@ -0,0 +1,284 @@
+import { ECPairFactory } from "ecpair";
+import assert from "node:assert";
+import {
+  _outpointsHash,
+  _ser32,
+  addressToPubkey,
+  createTransaction,
+  isPaymentCodeValid,
+  pubkeyToAddress,
+} from "../ts_src/output";
+import * as ecc from "tiny-secp256k1";
+import { describe, it, expect } from "vitest";
+
+import { uint8ArrayToHex } from "uint8array-extras";
+
+const ECPair = ECPairFactory(ecc);
+
+function exactMatch(a: string[], b: string[]): boolean {
+  const sortedA = a.sort();
+  const sortedB = b.sort();
+  return (
+    sortedA.length === sortedB.length &&
+    sortedA.every((value, index) => value === sortedB[index])
+  );
+}
+
+function matchSubset(generated: string[], expected: string[][]): boolean {
+  return expected.some((subArray) => exactMatch(generated, subArray));
+}
+
+type Expected = {
+  outputs: string[][];
+};
+
+type Sending = {
+  given: any;
+  expected: Expected;
+};
+
+type TestCase = {
+  comment: string;
+  sending: Sending[];
+};
+
+it("smoke test", () => {
+  assert.deepStrictEqual(createTransaction([], []), []);
+});
+
+it("2 inputs - 0 SP outputs (just a passthrough)", () => {
+  assert.deepStrictEqual(
+    createTransaction(
+      [
+        {
+          txid: "f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16",
+          vout: 0,
+          wif: ECPair.fromPrivateKey(
+            Buffer.from(
+              "1cd5e8f6b3f29505ed1da7a5806291ebab6491c6a172467e44debe255428a192",
+              "hex"
+            )
+          ).toWIF(),
+          utxoType: "p2wpkh",
+        },
+        {
+          txid: "a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d",
+          vout: 0,
+          wif: ECPair.fromPrivateKey(
+            Buffer.from(
+              "7416ef4d92e4dd09d680af6999d1723816e781c030f4b4ecb5bf46939ca30056",
+              "hex"
+            )
+          ).toWIF(),
+          utxoType: "p2wpkh",
+        },
+      ],
+      [
+        {
+          address: "3FiYaHYHQTmD8n2SJxVYobDeN1uQKvzkLe",
+          value: 22_333,
+        },
+        {
+          address: "3NaQS28rzijWrmy1o5npqZAxVCJPWGd2Xn",
+          // no value
+        },
+      ]
+    ),
+    [
+      {
+        address: "3FiYaHYHQTmD8n2SJxVYobDeN1uQKvzkLe",
+        value: 22_333,
+      },
+      {
+        address: "3NaQS28rzijWrmy1o5npqZAxVCJPWGd2Xn",
+        // no value
+      },
+    ]
+  );
+});
+
+it("2 inputs - 1 SP output, 1 legacy, 1change (should not rearrange order of inputs )", () => {
+  assert.deepStrictEqual(
+    createTransaction(
+      [
+        {
+          txid: "f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16",
+          vout: 0,
+          wif: ECPair.fromPrivateKey(
+            Buffer.from(
+              "1cd5e8f6b3f29505ed1da7a5806291ebab6491c6a172467e44debe255428a192",
+              "hex"
+            )
+          ).toWIF(),
+          utxoType: "p2wpkh",
+        },
+        {
+          txid: "a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d",
+          vout: 0,
+          wif: ECPair.fromPrivateKey(
+            Buffer.from(
+              "7416ef4d92e4dd09d680af6999d1723816e781c030f4b4ecb5bf46939ca30056",
+              "hex"
+            )
+          ).toWIF(),
+          utxoType: "p2wpkh",
+        },
+      ],
+      [
+        {
+          address: "3FiYaHYHQTmD8n2SJxVYobDeN1uQKvzkLe",
+          value: 11_111,
+        },
+        {
+          address:
+            "sp1qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgqjuexzk6murw56suy3e0rd2cgqvycxttddwsvgxe2usfpxumr70xc9pkqwv",
+          value: 22_222,
+        },
+        {
+          // no address, which should be interpreted as change
+          value: 33_333,
+        },
+      ]
+    ),
+    [
+      {
+        address: "3FiYaHYHQTmD8n2SJxVYobDeN1uQKvzkLe",
+        value: 11_111,
+      },
+      {
+        address:
+          "bc1pszgngkje7t5j3mvdw8xc5l3q7n28awdwl8pena6hrvxgg83lnpmsme6u6j", // unwrapped from SP
+        value: 22_222,
+      },
+      {
+        // no address, which should be interpreted as change
+        value: 33_333,
+      },
+    ]
+  );
+});
+
+it("SilentPayment._outpointHash() works", () => {
+  const A = ECPair.fromWIF(
+    "L4cJGJp4haLbS46ZKMKrjt7HqVuYTSHkChykdMrni955Fs3Sb8vq"
+  ).publicKey;
+  assert.deepStrictEqual(
+    uint8ArrayToHex(
+      _outpointsHash(
+        [
+          {
+            txid: "a2365547d16b555593e3f58a2b67143fc8ab84e7e1257b1c13d2a9a2ec3a2efb",
+            vout: 0,
+            wif: "",
+            utxoType: "p2wpkh",
+          },
+        ],
+        A
+      )
+    ),
+    "94d5923201f2f239e4d2d5a44239e0377325a343e4c068cfd078217adc663d7c"
+  );
+  assert.deepStrictEqual(
+    uint8ArrayToHex(
+      _outpointsHash(
+        [
+          {
+            txid: "f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16",
+            vout: 0,
+            wif: "",
+            utxoType: "non-eligible",
+          },
+          {
+            txid: "a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d",
+            vout: 0,
+            wif: "",
+            utxoType: "p2wpkh",
+          },
+        ],
+        A
+      )
+    ),
+    "3ea0693eeb0c7e848ad7b875f1998e9ed02905e88a6f5c45f25fa187b7f073d2"
+  );
+});
+
+it("SilentPayment._ser32() works", () => {
+  assert.strictEqual(uint8ArrayToHex(_ser32(0)), "00000000");
+  assert.strictEqual(uint8ArrayToHex(_ser32(1)), "00000001");
+  assert.strictEqual(uint8ArrayToHex(_ser32(444)), "000001bc");
+});
+
+it("can validate payment code", () => {
+  assert.ok(
+    isPaymentCodeValid(
+      "sp1qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgqjuexzk6murw56suy3e0rd2cgqvycxttddwsvgxe2usfpxumr70xc9pkqwv"
+    )
+  );
+  assert.ok(
+    isPaymentCodeValid(
+      "sp1qqgrz6j0lcqnc04vxccydl0kpsj4frfje0ktmgcl2t346hkw30226xqupawdf48k8882j0strrvcmgg2kdawz53a54dd376ngdhak364hzcmynqtn"
+    )
+  );
+  assert.ok(
+    isPaymentCodeValid(
+      "sp1qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgqjex54dmqmmv6rw353tsuqhs99ydvadxzrsy9nuvk74epvee55drs734pqq"
+    )
+  );
+  assert.ok(
+    isPaymentCodeValid(
+      "sp1qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgqsg59z2rppn4qlkx0yz9sdltmjv3j8zgcqadjn4ug98m3t6plujsq9qvu5n"
+    )
+  );
+  assert.ok(
+    isPaymentCodeValid(
+      "sp1qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgq7c2zfthc6x3a5yecwc52nxa0kfd20xuz08zyrjpfw4l2j257yq6qgnkdh5"
+    )
+  );
+
+  assert.ok(
+    !isPaymentCodeValid(
+      "sp1qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgq7c2zfthc6x3a5yecwc52nxa0kfd20xuz08zyrjpfw4l2j257yq6qgn"
+    )
+  ); // short a few symbols
+  assert.ok(!isPaymentCodeValid("sp1qq")); // short a few symbols
+  assert.ok(!isPaymentCodeValid("garbage"));
+  assert.ok(
+    !isPaymentCodeValid(
+      "sp2qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgq7c2zfthc6x3a5yecwc52nxa0kfd20xuz08zyrjpfw4l2j257yq6qgnkdh5"
+    )
+  ); // wrong prefix
+  assert.ok(
+    !isPaymentCodeValid(
+      "qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgqjuexzk6murw56suy3e0rd2cgqvycxttddwsvgxe2usfpxumr70xc9pkqwv"
+    )
+  ); // no prefix
+  assert.ok(
+    !isPaymentCodeValid(
+      "qqgste7k9hx0qftg6qmwlkqtwuy6cycyavzmzj85c6qdfhjdpdjtdgqjuexzk6murw56suy3e0rd2cgqvycxttddwsvgxe2usfpxumr70xc9pkqwv123"
+    )
+  ); // no prefix
+});
+
+it("can turn pubkey into taproot address", () => {
+  assert.strictEqual(
+    pubkeyToAddress(
+      "40ef293a8a0ebaf8b351a27d89ff4b5b3822a635e4afdca77a30170c363bafa3"
+    ),
+    "bc1pgrhjjw52p6a03v635f7cnl6ttvuz9f34ujhaefm6xqtscd3m473szkl92g"
+  );
+
+  expect(() => {
+    pubkeyToAddress(
+      "512040ef293a8a0ebaf8b351a27d89ff4b5b3822a635e4afdca77a30170c363bafa3"
+    );
+  }).toThrow(/has no matching Address/);
+});
+
+it("can turn taproot address into pubkey", () => {
+  assert.strictEqual(
+    addressToPubkey(
+      "bc1pgrhjjw52p6a03v635f7cnl6ttvuz9f34ujhaefm6xqtscd3m473szkl92g"
+    ),
+    "40ef293a8a0ebaf8b351a27d89ff4b5b3822a635e4afdca77a30170c363bafa3"
+  );
+});
---
+++ ts_src/output.ts
@@ -0,0 +1,251 @@
+import { ECPairFactory } from "ecpair";
+import { bech32m } from "bech32";
+import * as bitcoin from "bitcoinjs-lib";
+import * as ecc from "@noble/secp256k1";
+import secp from "secp256k1";
+
+import {
+  compareUint8Arrays,
+  concatUint8Arrays,
+  hexToUint8Array,
+  uint8ArrayToHex,
+} from "uint8array-extras";
+import * as becc from "tiny-secp256k1";
+import { createTaggedHash } from "./utility";
+import { decodeSilentPaymentAddress, hrpFromNetwork } from "./encode";
+
+const ECPair = ECPairFactory(becc);
+bitcoin.initEccLib(becc);
+
+export type UTXOType =
+  | "p2wpkh"
+  | "p2sh-p2wpkh"
+  | "p2pkh"
+  | "p2tr"
+  | "non-eligible";
+
+export type UTXO = {
+  txid: string;
+  vout: number;
+  wif: string;
+  utxoType: UTXOType;
+  value?: number;
+  witnessUtxo?: object;
+};
+
+export type Target = {
+  address?: string; // either address or payment code
+  value?: number;
+};
+
+export type SilentPaymentGroup = {
+  Bscan: Uint8Array;
+  BmValues: Array<[Uint8Array, number | undefined, number]>;
+};
+
+export function createTransaction(
+  utxos: UTXO[],
+  targets: Target[],
+  config: { network: bitcoin.Network } = { network: bitcoin.networks.bitcoin }
+): Target[] {
+  const ret: Target[] = new Array(targets.length);
+
+  const silentPaymentGroups: Array<SilentPaymentGroup> = [];
+  for (let i = 0; i < targets.length; i++) {
+    const target = targets[i];
+
+    const prefix = hrpFromNetwork(config.network);
+
+    if (!target.address?.startsWith(prefix)) {
+      ret[i] = target; // passthrough
+      continue;
+    }
+
+    const { scanKey: Bscan, spendKey: Bm } = decodeSilentPaymentAddress(
+      target.address,
+      config.network
+    );
+
+    // Addresses with the same Bscan key all belong to the same recipient
+    const recipient = silentPaymentGroups.find(
+      (group) => compareUint8Arrays(group.Bscan, Bscan) === 0
+    );
+    if (recipient) {
+      recipient.BmValues.push([Bm, target.value, i]);
+    } else {
+      silentPaymentGroups.push({
+        Bscan: Bscan,
+        BmValues: [[Bm, target.value, i]],
+      });
+    }
+  }
+  if (silentPaymentGroups.length === 0) return ret; // passthrough
+
+  const a = _sumPrivkeys(utxos);
+  // Get X and Y coordinate, public key, form the private key a
+
+  const A = new Uint8Array(ecc.getPublicKey(a, true));
+  const outpoint_hash = _outpointsHash(utxos, A);
+
+  // Generating Pmk for each Bm in the group
+  for (const group of silentPaymentGroups) {
+    // Bscan * a * outpoint_hash
+
+    const ecdh_shared_secret_step1 = new Uint8Array(
+      secp.privateKeyTweakMul(a, outpoint_hash)
+    );
+
+    const ecdh_shared_secret = new Uint8Array(
+      ecc.getSharedSecret(
+        ecdh_shared_secret_step1 as Uint8Array,
+        group.Bscan as Uint8Array,
+        true
+      ) as Uint8Array
+    );
+
+    let k = 0;
+    for (const [Bm, amount, i] of group.BmValues) {
+      const tk = createTaggedHash(
+        "BIP0352/SharedSecret",
+        concatUint8Arrays([ecdh_shared_secret, _ser32(k)])
+      );
+
+      // Let Pmk = tk·G + Bm
+
+      const Pmk = new Uint8Array(
+        becc.pointAdd(
+          Bm,
+          becc.pointFromScalar(tk as Uint8Array, true)!,
+          true
+        ) as Uint8Array
+      );
+
+      // Encode Pmk as a BIP341 taproot output
+      const address = pubkeyToAddress(
+        uint8ArrayToHex(Pmk.slice(1)),
+        config.network
+      );
+      const newTarget: Target = { address };
+      newTarget.value = amount;
+      ret[i] = newTarget;
+      k += 1;
+    }
+  }
+  return ret;
+}
+
+export function _outpointsHash(parameters: UTXO[], A: Uint8Array): Uint8Array {
+  const outpoints: Array<Uint8Array> = [];
+  for (const parameter of parameters) {
+    const txidBuffer = hexToUint8Array(parameter.txid).reverse();
+    const voutBuffer = new Uint8Array(_ser32(parameter.vout).reverse());
+    outpoints.push(new Uint8Array([...txidBuffer, ...voutBuffer]));
+  }
+  outpoints.sort((a, b) => compareUint8Arrays(a, b));
+  const smallest_outpoint = outpoints[0];
+  return createTaggedHash(
+    "BIP0352/Inputs",
+    concatUint8Arrays([smallest_outpoint, A])
+  );
+}
+
+/**
+ * Serializes a 32-bit unsigned integer i as a 4-byte big-endian
+ * @param i {number} The number to serialize
+ * @returns {Uint8Array} The serialized number
+ * @private
+ * */
+export function _ser32(i: number): Uint8Array {
+  const returnValue = new Uint8Array(4);
+  returnValue[0] = (i >> 24) & 0xff;
+  returnValue[1] = (i >> 16) & 0xff;
+  returnValue[2] = (i >> 8) & 0xff;
+  returnValue[3] = i & 0xff;
+  return returnValue;
+}
+
+/**
+ * Sums the private keys of the UTXOs
+ * @param utxos {UTXO[]}
+ * @returns {Uint8Array} The sum of the private keys
+ * @private
+ **/
+function _sumPrivkeys(utxos: UTXO[]): Uint8Array {
+  if (utxos.length === 0) {
+    throw new Error("No UTXOs provided");
+  }
+
+  const keys: Array<Uint8Array> = [];
+  for (const utxo of utxos) {
+    let key = ECPair.fromWIF(utxo.wif).privateKey!;
+    switch (utxo.utxoType) {
+      case "non-eligible":
+        // Non-eligible UTXOs can be spent in the transaction, but are not used for the
+        // shared secret derivation. Note: we don't check that the private key is valid
+        // for non-eligible utxos because its possible the sender is following a different
+        // signing protocol for these utxos. For silent payments eligible utxos, we require
+        // access to the private key.
+        break;
+      case "p2tr":
+        if (key === undefined) {
+          throw new Error("No private key found for eligible UTXO");
+        }
+
+        // For taproot, check if the seckey results in an odd y-value and negate if so
+        if (ecc.getPublicKey(key)![0] === 0x03) {
+          //@ts-ignore
+          key = new Uint8Array(ecc.utils.privateNegate(key));
+        }
+      case "p2wpkh":
+      case "p2sh-p2wpkh":
+      case "p2pkh":
+        if (key === undefined) {
+          throw new Error("No private key found for eligible UTXO");
+        }
+        keys.push(key);
+        break;
+    }
+  }
+
+  if (keys.length === 0) {
+    throw new Error("No eligible UTXOs with private keys found");
+  }
+
+  // summary of every item in array
+  const ret = keys.reduce((acc, key) => {
+    return new Uint8Array(ecc.utils.privateAdd(acc, key) as Uint8Array);
+  });
+
+  return ret;
+}
+
+export function isPaymentCodeValid(pc: string) {
+  try {
+    const result = bech32m.decode(pc, 118);
+    const version = result.words.shift();
+    if (version !== 0) {
+      return false;
+    }
+  } catch (_) {
+    return false;
+  }
+
+  return true;
+}
+
+export function pubkeyToAddress(
+  hex: string,
+  network = bitcoin.networks.bitcoin
+): string {
+  const publicKey = hexToUint8Array("5120" + hex);
+  return bitcoin.address.fromOutputScript(Buffer.from(publicKey), network);
+}
+
+export function addressToPubkey(
+  address: string,
+  network = bitcoin.networks.bitcoin
+): string {
+  return uint8ArrayToHex(
+    bitcoin.address.toOutputScript(address, network).subarray(2)
+  );
+}
